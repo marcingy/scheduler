dfc56e30bf8d70539813cd760e83154ae479cf78
marcingy <marcingy@gmail.com>
2014-11-17 10:04:07 -0700

    Issue pub-156 by marcingy, add cron processing

---
+++ src/Annotation/SchedulerCron.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\scheduler\Annotation\SchedulerCron.
+ */
+
+namespace Drupal\scheduler\Annotation;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Defines an Scheduler Cron annotation object.
+ *
+ * @see hook_media_entity_type_info_alter()
+ *
+ * @Annotation
+ */
+class SchedulerCron extends Plugin {
+}
